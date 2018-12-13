77c9a6a919f48b37b475a170ba10595d236b31eb
pavelsalfetkin <pavelsalf@yandex.ru>
2018-12-13 19:38:38 +0300

    test

--- js/main.js
+++ js/main.js
@@ -25,6 +25,9 @@ var switchToSection1 = function switchToSection1() {
 var switchToSection2 = function switchToSection2() {
 	container.style.transform = 'translate3d(0px, -100%, 0px)';
 	container.style.transition = 'all 700ms ease 0s';
+	container.addEventListener("transitionend", function() {
+		section2.addEventListener('wheel', switch2, false);
+	}, false);
 };
 
 var switchToSection3 = function switchToSection3() {
@@ -32,51 +35,70 @@ var switchToSection3 = function switchToSection3() {
 	container.style.transition = 'all 700ms ease 0s';
 };
 
+
+var switch1 = function switch1(e) {
+	e.preventDefault();
+	if (e.deltaY > 24) {
+		section2.removeEventListener('wheel', switch2, false);
+		switchToSection2();
+	}
+};
+
+var switch2 = function switch2(e) {
+	e.preventDefault();
+	if (e.deltaY < -24) {
+		switchToSection1();
+	}
+	else if (e.deltaY > 24) {
+		switchToSection3();
+	}
+};
+
+var switch3 = function switch3(e) {
+	e.preventDefault();
+	if (e.deltaY < -24) {
+		section2.removeEventListener('wheel', switch2, false);
+		switchToSection2();
+	}
+};
+
+
 function swipeFromSection1(e) {
 	e.preventDefault();
-	console.log("event.target", e.target);
 	this.startPoint = e.targetTouches[0].clientY;
 	this.endPoint;
-
+	this.addEventListener('touchend', function() { e.preventDefault() });
+	this.addEventListener('touchcancel', function() { e.preventDefault() });
 	this.addEventListener('touchmove', function (e) {
 		e.preventDefault();
 		this.endPoint = e.targetTouches[0].clientY;
 		if (this.startPoint > this.endPoint) switchToSection2();
 	}, false);
-
-	this.addEventListener('touchend', function() { e.preventDefault() });
-	this.addEventListener('touchcancel', function() { e.preventDefault() });
 }
 
 function swipeFromSection2(e) {
 	e.preventDefault();
-	console.log("event.target", e.target);
 	this.startPoint = e.targetTouches[0].clientY;
 	this.endPoint;
-
+	this.addEventListener('touchend', function() { e.preventDefault() });
+	this.addEventListener('touchcancel', function() { e.preventDefault() });
 	this.addEventListener('touchmove', function (e) {
 		this.endPoint = e.targetTouches[0].clientY;
 		if (this.startPoint > this.endPoint) switchToSection3();
 		else switchToSection1();
 	}, false);
-
-	this.addEventListener('touchend', function() { e.preventDefault() });
-	this.addEventListener('touchcancel', function() { e.preventDefault() });
 }
 
 function swipeFromSection3(e) {
 	e.preventDefault();
-	console.log("event.target", e.target);
 	this.startPoint = e.targetTouches[0].clientY;
 	this.endPoint;
-
+	this.addEventListener('touchend', function() { e.preventDefault() });
+	this.addEventListener('touchcancel', function() { e.preventDefault() });
 	this.addEventListener('touchmove', function (e) {
 		this.endPoint = e.targetTouches[0].clientY;
 		if (this.startPoint < this.endPoint) switchToSection2();
 	}, false);
-
-	this.addEventListener('touchend', function() { e.preventDefault() });
-	this.addEventListener('touchcancel', function() { e.preventDefault() });
 }
 
 
@@ -85,44 +107,15 @@ function swipeFromSection3(e) {
 /* ---------------------------------------------------------------------------- */
 
 
+section1.addEventListener('wheel', switch1, false);
+// section2.addEventListener('wheel', switch2, false);
+section3.addEventListener('wheel', switch3, false);
+
 section1.addEventListener('touchstart', swipeFromSection1, false);
 section2.addEventListener('touchstart', swipeFromSection2, false);
 section3.addEventListener('touchstart', swipeFromSection3, false);
 
-section1.addEventListener('wheel', function(e) {
-	e.preventDefault();
-	if (e.deltaY > 24) switchToSection2();
-});
-
-section2.addEventListener('wheel', function(e) {
-	e.preventDefault();
-	if (e.deltaY < -24) switchToSection1();
-	else if (e.deltaY > 24) switchToSection3();
-});
-
-section3.addEventListener('wheel', function(e) {
-	e.preventDefault();
-	if (e.deltaY < -24) switchToSection2();
-});
-
-section1.addEventListener('scroll', function(e) {
-	e.preventDefault();
-	if (e.deltaY > 24) switchToSection2();
-});
-
-section2.addEventListener('scroll', function(e) {
-	e.preventDefault();
-	if (e.deltaY < -24) switchToSection1();
-	else if (e.deltaY > 24) switchToSection3();
-});
-
-section3.addEventListener('scroll', function(e) {
-	e.preventDefault();
-	if (e.deltaY < -24) switchToSection2();
-});
-
-
-document.addEventListener("touchstart", function(e) { e.preventDefault() }, false);
-document.addEventListener("touchend", function(e) { e.preventDefault() }, false);
-document.addEventListener("touchcancel", function(e) { e.preventDefault() }, false);
-document.addEventListener("touchmove", function(e) { e.preventDefault() }, false);
+// document.addEventListener("touchstart", function(e) { e.preventDefault() }, false);
+// document.addEventListener("touchend", function(e) { e.preventDefault() }, false);
+// document.addEventListener("touchcancel", function(e) { e.preventDefault() }, false);
+// document.addEventListener("touchmove", function(e) { e.preventDefault() }, false);
