f5b2dcde8097a5a42a59d2bbae34798e8883b9bf
pavelsalfetkin <pavelsalf@yandex.ru>
2018-12-13 20:00:16 +0300

    section slider v.1

--- js/main.js
+++ js/main.js
@@ -19,12 +19,12 @@ var section3 = document.getElementById("section3");
 
 var switchToSection1 = function switchToSection1() {
 	container.style.transform = 'translate3d(0px, 0px, 0px)';
-	container.style.transition = 'all 700ms ease 0s';
+	container.style.transition = 'all 600ms ease 0s';
 };
 
 var switchToSection2 = function switchToSection2() {
 	container.style.transform = 'translate3d(0px, -100%, 0px)';
-	container.style.transition = 'all 700ms ease 0s';
+	container.style.transition = 'all 600ms ease 0s';
 	container.addEventListener("transitionend", function() {
 		section2.addEventListener('wheel', switch2, false);
 	}, false);
@@ -32,7 +32,7 @@ var switchToSection2 = function switchToSection2() {
 
 var switchToSection3 = function switchToSection3() {
 	container.style.transform = 'translate3d(0px, -200%, 0px)';
-	container.style.transition = 'all 700ms ease 0s';
+	container.style.transition = 'all 600ms ease 0s';
 };
 
 
@@ -114,8 +114,3 @@ section3.addEventListener('wheel', switch3, false);
 section1.addEventListener('touchstart', swipeFromSection1, false);
 section2.addEventListener('touchstart', swipeFromSection2, false);
 section3.addEventListener('touchstart', swipeFromSection3, false);
-
-// document.addEventListener("touchstart", function(e) { e.preventDefault() }, false);
-// document.addEventListener("touchend", function(e) { e.preventDefault() }, false);
-// document.addEventListener("touchcancel", function(e) { e.preventDefault() }, false);
-// document.addEventListener("touchmove", function(e) { e.preventDefault() }, false);
